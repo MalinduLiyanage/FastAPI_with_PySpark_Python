d63e1489d435e6fb0a9f91434f97607ae53fd5dc
Malindu Liyanage <136006504+MalinduLiyanage@users.noreply.github.com>
2025-06-09 16:42:43 +0530

    DTO changes for customer responses

--- app/api/v1/endpoints/customer_endpoints.py
+++ app/api/v1/endpoints/customer_endpoints.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from typing import List, Any
-from app.dtos.customerDTO import CustomerAnnualSalaryDTO
+from app.dtos.customer.annual_salary_DTO import CustomerAnnualSalaryDTO
 from app.dtos.responses.base_response import BaseResponse
 from app.services.customer_service.customer_service import process_customer_annual_salary
 from app.configs.spark_session import get_spark
